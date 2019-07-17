773ebcba497ab7519c2acc7c56dc020b8a43579c
TammyLaforest <TammyLaforest@gmail.com>
2019-07-17 08:45:58 -0400

    start of project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MovieTickets
+
+Learning to build React Native apps for IOS in Xcode, outside of Expo.
+
+https://rationalappdev.com/movie-tickets-booking-app-with-react-native/
---
+++ src/Movies.js
@@ -0,0 +1,16 @@
+import React, { component } from 'react'
+
+import { ScrollView, Text, View } from 'react-native'
+import { movies } from './data'
+
+export default class Movies extends Component {
+    render() {
+        return (
+            <View>
+                <ScrollView>
+                    {movies.map((movie, index) => <Text>{movie.title}</Text>)}
+                </ScrollView>
+            </View >
+        )
+    }
+}
